a30b2b153bb969d5e42d89d1ee09389e254d3fc4
yasir <donyasser4@gmail.com>
2021-10-27 19:57:13 +0300

    YASIR_HAKAMI

---
+++ Yasir_lab15/Yasir_lab15Tests/Yasir_lab15Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Yasir_lab15Tests.swift
+//  Yasir_lab15Tests
+//
+//  Created by Yasir Hakami on 27/10/2021.
+//
+
+import XCTest
+@testable import Yasir_lab15
+
+class Yasir_lab15Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
