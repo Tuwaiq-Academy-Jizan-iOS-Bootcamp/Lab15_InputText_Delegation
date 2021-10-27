f4d58ae7dbfc3cd323e47167d76b27c431429ed8
maram <maramfaqih92@gmail.com>
2021-10-27 21:04:03 +0300

    Lab 15 : madam

---
+++ MaramFaqih_Lab15_InputText_Delegation/MaramFaqih_Lab15_InputText_Delegation/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MaramFaqih_Lab15_InputText_Delegation
+//
+//  Created by meme f on 21/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelForDisplay: UILabel!
+    
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func displayButton(_ sender: UIButton) {
+       let fName = firstNameField.text! 
+        let lName = lastNameField.text!
+        
+        labelForDisplay.text =  "My Name is \(fName) \(lName)"
+        
+    }
+       
+   
+    
+}
+
+extension ViewController:UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        labelForDisplay.resignFirstResponder()
+        return true
+    }
+}
