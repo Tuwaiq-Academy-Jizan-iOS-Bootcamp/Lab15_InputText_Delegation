24bd5c338f8fd6b9bdfa48f8f72ade0c3686779b
Faten Abdullh salem <faten@MacBook-Pro-Faten.local>
2021-10-28 08:48:27 +0300

    Lab15

---
+++ AA/AA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AA
+//
+//  Created by Faten Abdullh salem on 21/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
