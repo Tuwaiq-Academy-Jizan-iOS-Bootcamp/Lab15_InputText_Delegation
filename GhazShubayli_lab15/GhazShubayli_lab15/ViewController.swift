81d8280156658286073098d611eae157b84f4c57
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2021-10-28 08:53:12 +0300

    Ghazi Shubayli

---
+++ GhazShubayli_lab15/GhazShubayli_lab15/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GhazShubayli_lab15
+//
+//  Created by ماك بوك on 21/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var firstTextField: UITextField!
+  @IBOutlet weak var secondTextField: UITextField!
+  @IBOutlet weak var viewText: UILabel!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    firstTextField.delegate = self
+    secondTextField.delegate = self
+  }
+  @IBAction func buttonDisplay(_ sender: Any) {
+    viewText.text = "Your Name is \(firstTextField.text!) \(secondTextField.text!)"
+  }
+}
+extension ViewController: UITextFieldDelegate{
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    firstTextField.resignFirstResponder()
+    secondTextField.resignFirstResponder()
+    return true
+  }
+}
+
---
+++ GhazShubayli_lab15/GhazShubayli_lab15Tests/GhazShubayli_lab15Tests.swift
@@ -0,0 +1,33 @@
+//
+//  GhazShubayli_lab15Tests.swift
+//  GhazShubayli_lab15Tests
+//
+//  Created by ماك بوك on 21/03/1443 AH.
+//
+
+import XCTest
+@testable import GhazShubayli_lab15
+
+class GhazShubayli_lab15Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
